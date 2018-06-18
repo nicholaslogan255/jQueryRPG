d5ad9f06d2a29e040a12a686c223375bc0c194a1
Nic_Logan <nicholaslogan255@gmail.com>
2018-06-17 21:48:01 -0500

    Basic Formatting

---
+++ assets/javascript/rpg.js
@@ -0,0 +1,17 @@
+var playerMaxHp = 10;
+var playercurHp = 10;
+
+
+
+// code runs here
+$(document).ready(function() {
+
+    // when random-button is clicked
+    $("#random-button").on("click", function() {
+
+      // replace randombuttons text
+      $("#random-number").text("foobar");
+
+    });
+
+  });
